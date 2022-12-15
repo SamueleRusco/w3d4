57266f15dad2780f93bbce2ba87169db2e0b88ee
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2022-12-15 18:00:40 +0100

    parzialmente finito

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
-additem () => {
+const aggiungiClasseSelected = (event) => {
+  const elementoCliccato = event.target;
+  elementoCliccato.classList.toggle("selected");
+};
 
-}
+const inserisciTesto = (event) => {
+  if (event.key === "Enter") {
+    const valoreInputField = document.getElementById("inputText").value;
+    console.log(valoreInputField);
+    if (valoreInputField === "") {
+      alert("inserisci qualcosa");
+    } else {
+      const nuovoElementoLista = document.createElement("div");
+      nuovoElementoLista.classList.add("list-item");
 
+      nuovoElementoLista.innerText = valoreInputField;
+      nuovoElementoLista.onclick = aggiungiClasseSelected;
+      document.getElementById("lista").append(nuovoElementoLista);
+      document.getElementById("inputText").value = "";
+    }
+  }
+};
